981317b238c2e3e9e76c5ce7b2ec798029810dcf
Yael-Munoz <martin_yael@icloud.com>
2025-11-04 17:02:52 -0800

    no tengo tiempo, pero es update de backedn

--- server/routes/register.js
+++ server/routes/register.js
@@ -5,8 +5,50 @@ const Pro = require('../models/Pro');
 
 router.post('/', (req, res) => {
     const data = req.body;
-    console.log('Client data received: ', data);
-    res.status(200).json({message: 'Data was received: ', data});
+
+    const errors = [];
+
+    if(!data.role) {errors.push('Role is required');}
+
+    if(!data.username 
+       || data.username.length < 4
+       || data.username.length > 16) 
+       {errors.push('Username is required and must be between 4 and 16 characters');}
+
+    if(!data.password
+       || !data.confirmPassword 
+       || data.password 
+       !== data.confirmPassword) {errors.push('Password is required and must match the confirmation password');}
+
+    let isProValid = true;
+
+    if(!data.name) {errors.push('Name is required'); isProValid = false;}
+    if(!data.number) {errors.push('Phone number is required'); isProValid = false;}
+    if(!data.email) {errors.push('Email is required'); isProValid = false;}
+
+    const role = data.role;
+    const username = data.username;
+    const password = data.password;
+    const confirmPassword = data.confirmPassword;
+
+    if(isProValid) {
+        const name = data.name;
+        const number = data.number;
+        const email = data.email;
+    }
+
+    if(errors.length > 0) {
+        console.log('Error(s) were captured: ' + errors);
+        return res.status(400).json({message: errors});
+    }
+
+
+    
+
+    
+
+
+
 });
 
 
