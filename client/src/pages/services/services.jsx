9b9648a97ce9ee154998ff0ed70197479c73264a
Drolord22 <exxon.giselle@gmail.com>
2025-11-06 03:23:13 -0800

    Update pagina servicios
    - Se mejoro diseño general de la página de servicios
    - Se cambio a un fondo animado
    - Se agregaron imagenes y tarjetas de servicios
    - Aun no esta terminada

--- client/src/pages/services/services.jsx
+++ client/src/pages/services/services.jsx
@@ -1,6 +1,12 @@
 import { useNavigate } from 'react-router-dom';
 import NavCenter from '../../components/navbar-center/navbar-center.jsx';
 import styles from './services.module.css';
+import doctor from "../../assets/services-page/doctor.jpg";
+import dentista from "../../assets/services-page/dentista.webp"
+import barbero from "../../assets/services-page/barbero.jpg"
+import nutriologo from "../../assets/services-page/nutriologo.jpg"
+import cli from "../../assets/services-page/cli.png"
+import pro from "../../assets/services-page/pro.png"
 
 function Services() {
   const navigate = useNavigate();
@@ -19,41 +25,58 @@ function Services() {
 
         <section className={styles['servicios-grid']}>
           <div className={styles['servicio-card-doctor']} data-aos="fade-up">
-            <i className="fa-solid fa-user-doctor"></i>
+            <div className={styles['img-wrapper-doctor']}>
+              <img src={doctor} alt="doctor" className={styles['servicio-img']} />
+            </div>
             <h3>Doctores</h3>
             <p>Consulta médica general, especialistas y seguimiento clínico.</p>
           </div>
           <div className={styles['servicio-card-dentista']} data-aos="fade-up" data-aos-delay="100">
-            <i className="fa-solid fa-tooth"></i>
+            <div className={styles['img-wrapper-dentista']}>
+              <img src={dentista} alt="dentista" className={styles['servicio-img']} />
+            </div>
             <h3>Dentistas</h3>
             <p>Odontología preventiva, estética y tratamientos especializados.</p>
           </div>
           <div className={styles['servicio-card-barbero']} data-aos="fade-up" data-aos-delay="200">
-            <i className="fa-solid fa-scissors"></i>
+            <div className={styles['img-wrapper-barbero']}>
+              <img src={barbero} alt="barbero" className={styles['servicio-img']} />
+            </div>
             <h3>Barberos</h3>
             <p>Estilo personalizado, cortes modernos y cuidado de la barba.</p>
           </div>
           <div className={styles['servicio-card-nutriologo']} data-aos="fade-up" data-aos-delay="300">
-            <i className="fa-solid fa-apple-whole"></i>
+            <div className={styles['img-wrapper-nutriologo']}>
+              <img src={nutriologo} alt="nutriologo" className={styles['servicio-img']} />
+            </div>
             <h3>Nutriólogos</h3>
             <p>Planes alimenticios, control de peso y asesoría nutricional.</p>
           </div>
         </section>
 
-        <section className ={styles['cta-cli']} data-aos= "fade-in">
-            <h2>¿Buscas agendar citas?</h2>
-            <p>Regístrate y empieza a agendar con tiempo tus pendientes desde nuestra plataforma</p>
-            <button className={styles['cta-button-cli']} onClick={() => navigate('/register')}>
-            Crear cuenta personal
-          </button>
+        <section className ={styles['cta-cli']} data-aos="fade-down" data-aos-delay="500">
+          <div className={styles['img-wrapper-cli']}>
+            <img src={cli} alt="cli" className={styles['servicio-img-cli']} />
+          </div>
+          <div className={styles["cta-cli-contenido"]}>
+              <h2>¿Buscas agendar citas?</h2>
+              <p>Regístrate y empieza a agendar con tiempo tus pendientes desde nuestra plataforma</p>
+              <button className={styles['cta-button-cli']} onClick={() => navigate('/register')}>
+              Crear cuenta personal
+              </button>
+          </div>
         </section>
 
-        <section className={styles['cta-pro']} data-aos="fade-in">
+        <section className={styles['cta-pro']}>
           <h2>¿Eres profesional?</h2>
           <p>Regístrate y gestiona tus horarios, clientes y citas desde nuestra plataforma.</p>
           <button className={styles['cta-button-pro']} onClick={() => navigate('/register')}>
             Crear cuenta profesional
           </button>
+          <div className={styles['img-wrapper-pro']}>
+            <img src={pro} alt="pro" className={styles['servicio-img-pro']} />
+          </div>
+          
         </section>
       </div>
     </div>
